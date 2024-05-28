568d2a70f9c26c5a54fe2af988651ca4a40773e1
msuzuki90 <muazu.suzuki@gmail.com>
2024-05-28 16:25:26 +0200

    started applying style with Tailwind

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,6 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
-import './bootstrap';
+import './bootstrap.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
