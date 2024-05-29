e99e3c7de25bd0ef411c38c06280d73fdc1f1167
msuzuki90 <muazu.suzuki@gmail.com>
2024-05-29 15:41:30 +0200

    changed webpack configuration and installed stimulus bundle

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,15 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/styles/app.scss')
+    .addEntry('app', './assets/app.js')
+
+    //seperate entry point for scss
+    .addStyleEntry('styles/app', './assets/styles/app.scss')
+
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
+
+
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
