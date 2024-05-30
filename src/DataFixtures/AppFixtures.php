370fcb8462d42acdd5d21ea0ea783a27100dd1ae
msuzuki90 <muazu.suzuki@gmail.com>
2024-05-30 20:57:00 +0200

    Setup and Testing user password Hasher in appfixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,50 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    ){
+    }
+
+
     public function load(ObjectManager $manager): void
     {
+        $user1= new User();
+        $user1->setEmail('rapper@mail.com');
+        $user1->setPassword(
+            $this->userPasswordHasher->hashPassword($user1,'tupac')
+        );
+        $manager->persist($user1);
+
+        $user2= new User();
+        $user2->setEmail('rock@mail.com');
+        $user2->setPassword(
+            $this->userPasswordHasher->hashPassword($user2,'Metallica')
+        );
+        $manager->persist($user2);
+
+        $user3 = new User();
+        $user3->setEmail('reggae@mail.com');
+        $user3->setPassword(
+            $this->userPasswordHasher->hashPassword($user3,'Marley')
+        );
+        $manager->persist($user3);
+
+        $user4 = new User();
+        $user4->setEmail('pop@mail.com');
+        $user4->setPassword(
+            $this->userPasswordHasher->hashPassword($user4,'Swift')
+        );
+        $manager->persist($user4);
+        
         $microPost1 = new MicroPost;
         $microPost1->setTitle('Kendrick Lamar');
         $microPost1->setText('The Modern King Kunta');
