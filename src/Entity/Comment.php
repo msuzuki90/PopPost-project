881ab1eaf81a4e005ce838811a726c6bb4a318f2
msuzuki90 <muazu.suzuki@gmail.com>
2024-05-30 14:18:23 +0200

    added validator constraint in Comment entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 class Comment
@@ -25,6 +26,8 @@ class Comment
         return $this->id;
     }
 
+    #[Assert\NotBlank()]
+    #[Assert\Length(min:5,max:500,minMessage:'Comment is way to short! 5 character minimum pretty please.')]
     public function getText(): ?string
     {
         return $this->Text;
