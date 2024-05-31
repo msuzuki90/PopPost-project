ae30dca6b2c5abaf67f838403f985c2392955f90
msuzuki90 <muazu.suzuki@gmail.com>
2024-05-31 17:08:15 +0200

    managed to fix migration issue and created migration. (Created datetime property in Comment entity)

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -24,6 +25,9 @@ class Comment
     #[ORM\ManyToOne(inversedBy: 'comments')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $Author = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $Created = null;
 
     public function getId(): ?int
     {
@@ -64,6 +68,18 @@ class Comment
     public function setAuthor(?User $Author): static
     {
         $this->Author = $Author;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->Created;
+    }
+
+    public function setCreated(\DateTimeInterface $Created): static
+    {
+        $this->Created = $Created;
 
         return $this;
     }
