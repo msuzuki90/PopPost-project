efde8adb52f97e0cebc46d526a63c14aa659ac51
msuzuki90 <muazu.suzuki@gmail.com>
2024-06-03 19:58:35 +0200

    created form for profile setting and configured it in SettingProfileController

---
+++ src/Form/UserProfileType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\UserProfile;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Name')
+            ->add('Bio')
+            ->add('WebsiteUrl')
+            ->add('TwitterUsername')
+            ->add('Company')
+            ->add('Location')
+            ->add('DateOfBirth', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => UserProfile::class,
+        ]);
+    }
+}
