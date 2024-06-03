ea347d89e4e80aa3eb3dfd0064db4d784e3666ab
msuzuki90 <muazu.suzuki@gmail.com>
2024-06-03 20:36:30 +0200

    configured flash message and route in SettingsProfileController

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -5,16 +5,20 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SettingsProfileController extends AbstractController
 {
     #[Route('/settings/profile', name: 'app_settings_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function profile(
-        Request $request
+        Request $request,
+        EntityManagerInterface $entityManager,
     ): Response
     {
         /** @var User $user */
@@ -28,11 +32,16 @@ class SettingsProfileController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()){
             $userProfile = $form->getData();
+            $user->setUserProfile($userProfile);
+            $entityManager->persist($user);
+            $entityManager->flush();
 
             //add Flash
-
+            $this->addFlash('success','Nice, your user profile has been saved!');
             //redirect route
-
+            return $this->redirectToRoute(
+                'app_settings_profile'
+            );
 
         }
 
