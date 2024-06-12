527bf0543369cba2499bd8c95c1c5a117425f5e5
msuzuki90 <muazu.suzuki@gmail.com>
2024-06-12 15:58:40 +0200

    created image column in UserProfile entity

--- src/Entity/UserProfile.php
+++ src/Entity/UserProfile.php
@@ -39,6 +39,9 @@ class UserProfile
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Image = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -136,6 +139,18 @@ class UserProfile
     public function setUser(User $user): static
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(?string $Image): static
+    {
+        $this->Image = $Image;
 
         return $this;
     }
