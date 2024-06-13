5d1cca124e1d6756b4272b3dd991bd02ab53c99e
msuzuki90 <muazu.suzuki@gmail.com>
2024-06-13 10:26:23 +0200

    configured Tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   darkMode:'class',
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],
+  safelist: [
+    'h-16', 'w-16',
+    'h-24', 'w-24',
+    'h-32', 'w-32',
+    'h-40', 'w-40',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '16': '4rem',
+        '24': '6rem',
+        '32': '8rem',
+        '40': '10rem',
+        // Add other sizes as necessary
+      },
+    },
   },
+
   plugins: [],
 }
 
