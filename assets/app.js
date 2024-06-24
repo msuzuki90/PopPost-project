93a2b83931d16bb2bc3acd78bd55e36e83577e05
msuzuki90 <muazu.suzuki@gmail.com>
2024-06-24 14:33:31 +0200

    setup for dark/light mode

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,65 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
+
+
+// Icons
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
+
+//Theme Vars
+
+const userTheme = localStorage.getItem("theme");
+
+//Icon Toggling
+
+const iconToggle = () => {
+    moonIcon.classList.toggle("display-none");
+    sunIcon.classList.toggle("display-none"); 
+};
+
+//Initial Theme check
+const themeCheck = () => {
+    if(userTheme === "dark" || !userTheme){
+        document.documentElement.classList.add("dark");
+        moonIcon.classList.add("display-none");
+        return
+    } 
+    sunIcon.classList.add("display-none");
+}
+
+// Manual Theme Switch
+
+const themeSwitch = () => {
+    if(document.documentElement.classList.contains("dark")){
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem("theme","dark");
+        iconToggle;
+    };
+}
+
+// Call theme switch on clicking buttons
+sunIcon.addEventListener("click",() => {
+    themeSwitch();
+});
+
+moonIcon.addEventListener("click", ()=>{
+    themeSwitch();
+});
+
+
+// invoke theme check on initial load
+themeCheck();
+
+
+
+
+
+
+
 import './styles/app.scss';
 import './bootstrap';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
