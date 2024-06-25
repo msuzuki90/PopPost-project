f6323b31f05af02c713d884985daeaec71283cc6
msuzuki90 <muazu.suzuki@gmail.com>
2024-06-25 15:52:31 +0200

    created and configured paginator bundle - configured easyadmin

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+
+            EmailField::new('email'),
+            ChoiceField::new('roles')->setChoices([
+                'USER' => 'ROLE_COMMENTER',
+                'ADMIN' => 'ROLE_EDITOR',
+            ])->allowMultipleChoices(),
+        ];
+    }
+    
+}
