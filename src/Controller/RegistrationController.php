f153e60bfb6717e2b6f19e1741fe0b4202f76f15
msuzuki90 <muazu.suzuki@gmail.com>
2024-07-14 09:56:30 +0200

    configured inscription and registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -45,7 +45,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('accounts@poppost.com', 'PopPost Mail Bot'))
+                    ->from(new Address('contact@paic-france.com', 'PopPost Mail Bot'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
